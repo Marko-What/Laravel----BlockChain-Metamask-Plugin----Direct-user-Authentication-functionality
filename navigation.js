ab60fe3ec95eea15395b3571e143f2030092fde0
Marko-What <markoFindJob@gmail.com>
2018-02-22 14:21:11 +0000

    Delete navigation.js

--- navigation.js
+++
@@ -1,8 +0,0 @@
-	$("#navigation #box").on("click", function(){
-		
-		$(this).css("background-color", "white").siblings().css("background-color", "#efefef");
-		var value = $(this).attr('data-file');
-	/*	console.log(value);*/
-		$(value).addClass('active').siblings().removeClass('active');
-		
-	});
